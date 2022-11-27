e53736be433dc8c9ed2ccf96590343aaf22a2567
zkariaait <zakaria.ait.11@gmail.com>
2022-11-27 21:37:56 +0000

    app routing

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 
 
-export const routes: Routes = [
+import { LoginComponent } from '../login/login.component';
+import { RegisterComponent } from '../register/register.component';
 
-    
+export const routes: Routes = [
+  { path: 'login',     component: LoginComponent },
+  { path: 'signup',  component: RegisterComponent }
+  
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { RegisterComponent } from './register/register.component';
   imports: [
     BrowserModule,
     AppRoutingModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
