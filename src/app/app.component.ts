2b40af895a1a21537ceb8bc83dfd7a854740e176
AnassNebdaoui <nebdaoui.anass@gmail.com>
2022-12-19 21:54:50 +0100

    Route propriétaire stade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Routes } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'F2R_FrontEnd';
-  
 }
