0d54e640cf9cbe3b849a2651ffcfe2534dfeedf3
zkariaait <zakaria.ait.11@gmail.com>
2022-12-25 03:00:34 +0000

    Stade Admin

---
+++ src/app/proprietaire/add-stade/add-stade.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-stade',
+  templateUrl: './add-stade.component.html',
+  styleUrls: ['./add-stade.component.scss']
+})
+export class AddStadeComponent {
+
+}
