6c99489a091a30ce5eccb6622db2aa6405660f53
zkariaait <zakaria.ait.11@gmail.com>
2023-01-05 21:38:39 +0000

    Annonces des Résérvations

--- src/app/annonces/annonces.component.ts
+++ src/app/annonces/annonces.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { ReservationService } from '../services/reservation.service';
+import { Reservation } from '../shared/reservation';
 
 @Component({
   selector: 'app-annonces',
   templateUrl: './annonces.component.html',
   styleUrls: ['./annonces.component.scss']
 })
-export class AnnoncesComponent {
+export class AnnoncesComponent implements OnInit{
+
+  annonces: Reservation[]
+  constructor(private reservationService: ReservationService )  { }
+
+
+  ngOnInit(): void {
+
+   this.reservationService.getAnnonces().subscribe(annonces => this.annonces = annonces );
+   
+   
+
+  }
 
 }
